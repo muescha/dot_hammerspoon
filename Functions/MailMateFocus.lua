e099d14dfe9eff6034e18cfe87759692327f26f7
muescha <184316+muescha@users.noreply.github.com>
2023-01-29 02:41:45 +0100

    MailMateFocus: add option to have with cmd+click and normal click

--- Functions/MailMateFocus.lua
+++ Functions/MailMateFocus.lua
@@ -15,13 +15,37 @@ local chromeBundleID = "com.google.Chrome"
 -- Define a function to check if the current app is MailMate and the cmd key was pressed while clicking a link
 local function checkCmdClickInMailmateAndActivateMailmate()
 
+    -- Setup the click behaviour:
+    -- -> use normal click to come back: false
+    -- -> use cmd+click to come back: true
+    local enable_cmdClick = true
+
     if hs.application.frontmostApplication():bundleID() ~= mailmateBundleID then
-        --debugInfo(scriptInfo, 'not in mailmate --> exit')
+        --debugInfo(scriptInfo, 'not in MailMate --> exit')
         return false
     end
-    if not hs.eventtap.checkKeyboardModifiers()['cmd'] then
-        debugInfo(scriptInfo, 'no modifier `cmd` --> exit')
-        return false
+
+    if enable_cmdClick then
+        if not hs.eventtap.checkKeyboardModifiers()['cmd'] then
+            debugInfo(scriptInfo, 'no modifier `cmd` --> exit')
+            return false
+        end
+    else
+        local mousePos = hs.mouse.getAbsolutePosition()
+        --debugInfo(scriptInfo,'mousePos ',mousePos)
+        local focusedWindow = hs.window.focusedWindow()
+        --debugInfo(scriptInfo,'focusedWindow ',focusedWindow)
+        local frame = focusedWindow:frame()
+        --debugInfo(scriptInfo,'frame ',frame)
+        if mousePos.x < frame.x or mousePos.y < frame.y or
+                mousePos.x > frame.x + frame.w or mousePos.y > frame.y + frame.h then
+            -- Click occurred outside the window
+            debugInfo(scriptInfo, "Click outside MailMate window --> exit")
+            return false
+        else
+            -- Click occurred inside the MailMate window
+            --debugInfo(scriptInfo, "Click inside MailMate window")
+        end
     end
 
     hs.timer.doAfter(0.5, function()
