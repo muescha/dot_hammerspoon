abfd89f5e16f62419216e46de14830a106c03abe
muescha <184316+muescha@users.noreply.github.com>
2024-03-19 21:14:26 +0100

    SetupLuaRocks.lua - typo in variable name

--- Functions/SetupLuaRocks.lua
+++ Functions/SetupLuaRocks.lua
@@ -5,11 +5,11 @@ local lVer = _VERSION:match("Lua (.+)$")
 --local luarocks = hs.execute("which luarocks"):gsub("\n", "")
 local luarocks = "/Users/muescha/.asdf/shims/luarocks"
 
-local loarocks_path = hs.execute(luarocks .. " --lua-version " .. lVer .. " path --lr-path"):gsub("\n", "")
-local loarocks_cpath = hs.execute(luarocks .. " --lua-version " .. lVer .. " path --lr-cpath"):gsub("\n", "")
+local luarocks_path = hs.execute(luarocks .. " --lua-version " .. lVer .. " path --lr-path"):gsub("\n", "")
+local luarocks_cpath = hs.execute(luarocks .. " --lua-version " .. lVer .. " path --lr-cpath"):gsub("\n", "")
 
-package.path = package.path .. ";" .. loarocks_path
-package.cpath = package.cpath .. ";" .. loarocks_cpath
+package.path = package.path .. ";" .. luarocks_path
+package.cpath = package.cpath .. ";" .. luarocks_cpath
 
 --print(package.path:gsub(';', ";\n"))
 --print(package.cpath:gsub(';', ";\n"))
