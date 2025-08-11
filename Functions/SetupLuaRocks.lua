2f7940fb593ca310bb57bf6fc06fed70329bf4c1
muescha <184316+muescha@users.noreply.github.com>
2025-08-11 16:34:23 +0200

    SetupLuaRocks.lua: Refactor command execution for improved readability and maintainability

--- Functions/SetupLuaRocks.lua
+++ Functions/SetupLuaRocks.lua
@@ -7,12 +7,28 @@ local lVer = _VERSION:match("Lua (.+)$")
 --local luarocks = hs.execute("which luarocks"):gsub("\n", "")
 local luarocks = "/Users/muescha/.asdf/shims/luarocks"
 
-local luarocks_path = hs.execute(luarocks .. " --lua-version " .. lVer .. " path --lr-path"):gsub("\n", "")
-local luarocks_cpath = hs.execute(luarocks .. " --lua-version " .. lVer .. " path --lr-cpath"):gsub("\n", "")
+local cmd_path = luarocks .. " --lua-version " .. lVer .. " path --lr-path "
+local cmd_cpath = luarocks .. " --lua-version " .. lVer .. " path --lr-cpath"
+
+local luarocks_path = hs.execute(cmd_path, true):gsub("\n", "")
+local luarocks_cpath = hs.execute(cmd_cpath, true):gsub("\n", "")
 
 package.path = package.path .. ";" .. luarocks_path
 package.cpath = package.cpath .. ";" .. luarocks_cpath
 
+--print("Luarocks Paths:")
+--local cmd_check = 'zsh -l -c "which /Users/muescha/.asdf/shims/luarocks"'
+--print(hs.execute(cmd_check))
+--local cmd_test = 'zsh -l -c "/Users/muescha/.asdf/shims/luarocks --lua-version ' .. lVer .. ' list"'
+--print(hs.execute(cmd_test))
+--print("lVer: ".. _VERSION)
+--print("lVer: ".. lVer)
+--print("cmd_path:  ".. cmd_path)
+--print("cmd_cpath: ".. cmd_cpath)
+--print(hs.execute(cmd_path, true))
+--print(hs.execute(cmd_cpath, true))
+--print("luarocks_path:  " .. luarocks_path)
+--print("luarocks_cpath: " .. luarocks_cpath)
 --print(package.path:gsub(';', ";\n"))
 --print(package.cpath:gsub(';', ";\n"))
 
